ab3ee3a8636aae6cf654ec52ffb79f6c155ef38c
aljand <aljand321@gmail.com>
2018-05-29 01:19:53 -0400

    modificando un poco

---
+++ app/src/main/java/com/example/ale_j/provandocongit2/vista.java
@@ -0,0 +1,15 @@
+package com.example.ale_j.provandocongit2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class vista extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vista);
+
+
+    }
+}
